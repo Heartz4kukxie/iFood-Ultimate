eb36d08de153bedfafee6be4a11613be6357fa3f
Heartz4kukxie <kuukuaeshun0@gmail.com>
2025-12-07 14:04:02 +0000

    Add constructor for Cheese class

--- src/main/java/data/iFood/Cheese.java
+++ src/main/java/data/iFood/Cheese.java
@@ -2,6 +2,8 @@ package data.iFood;
 
 public class Cheese extends FoodItem {
     // super class incantation in the constructor
+
+     // Constructor for Cheese objects
     public Cheese() {
         super("Cheese", 35, 15,0);
     }
