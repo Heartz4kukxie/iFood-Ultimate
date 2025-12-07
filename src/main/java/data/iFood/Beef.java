a1579ae75eba6cd6d5dce8c0712a36bc54119e61
Heartz4kukxie <kuukuaeshun0@gmail.com>
2025-12-07 14:02:41 +0000

    Add constructor to Beef class

--- src/main/java/data/iFood/Beef.java
+++ src/main/java/data/iFood/Beef.java
@@ -2,6 +2,8 @@ package data.iFood;
 
 public class Beef extends FoodItem {
     // super class incantation in the constructor
+    
+    // Constructor for Beef objects
     public Beef() {
         super("Beef",25.0,12.5,0);
     }
